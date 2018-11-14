63f48f4eafed71a559ae4dfb2e59b13be5e5f4cc
Sergey <ser-stulin20116@yandex.ru>
2018-11-14 22:16:17 +0500

    добавил строгое сравнение и заменил elseif на else

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-$is_auth = rand(0, 1);
-
+$is_auth = rand(0,1);
 $user_name = 'Sergey'; // укажите здесь ваше имя
 $user_avatar = 'img/user.jpg';
 ?>
@@ -29,14 +28,14 @@ $user_avatar = 'img/user.jpg';
 
         <nav class="user-menu">
 
-        <?php if ($is_auth == 1 ):?>
+        <?php if ($is_auth === 1 ): ?>
         <div class="user-menu__image">
             <img src="img/user.jpg" width="40" height="40" alt="Пользователь">
         </div>
         <div class="user-menu__logged">
             <p><?=$user_name;?></p>
         </div>
-        <?php elseif ($is_auth == 0 ):?>
+        <?php else: ?>
             <ul class="user-menu__list">
                 <li class="user-menu__item">
                     <a href="#">Регистрация</a>
@@ -45,7 +44,7 @@ $user_avatar = 'img/user.jpg';
                     <a href="#">Вход</a>
                 </li>
             </ul>
-        <? endif; ?>
+            <?php endif; ?>
         </nav>
     </div>
 </header>
