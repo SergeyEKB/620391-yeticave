691a3c1f7882c6908a475dce4bfe11599dc453ed
Sergey <ser-stulin20116@yandex.ru>
2018-11-23 20:45:41 +0500

    Добавил часы

--- templates/index.php
+++ templates/index.php
@@ -42,44 +42,55 @@ function priceform($price)
     }
     return $number.' <b class="rub">р</b>';
 }
+
+$ts = time();/*текущее время*/
+$newday = "24 November 2018";
+$end_ts  = strtotime($newday);
+$ts_diff = $end_ts-$ts;/*Показывает сколько осталось до начала нового дня в секундах*/
+$hours = floor($ts_diff / 3600);
+$minutes = floor(($ts_diff % 3600) / 60);
+if ($hours < 10){
+  $hours = '0'.$hours;
+};
 ?>
+
 <main class="container">
-<section class="promo">
-    <h2 class="promo__title">Нужен стафф для катки?</h2>
-    <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
-    <ul class="promo__list">
+  <section class="promo">
+      <h2 class="promo__title">Нужен стафф для катки?</h2>
+      <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
+      <ul class="promo__list">
         <?php foreach ($categories as $index => $category ): ?>
-        <li class="promo__item promo__item--boards">
-            <a class="promo__link" href="pages/all-lots.html<?=$index;?>"><?= $category;?></a>
-        </li>
-    <?php endforeach; ?>
-    </ul>
-</section>
-<section class="lots">
-    <div class="lots__header">
-        <h2>Открытые лоты</h2>
-    </div>
-    <ul class="lots__list">
-        <?php foreach ($lot_list as $lot ): ?>
-        <li class="lots__item lot">
-            <div class="lot__image">
-                <img src="<?=$lot['picture']; ?>" width="350" height="260" alt="">
-            </div>
-            <div class="lot__info">
-                <span class="lot__category"><?=$lot['category']; ?></span>
-                <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$lot['title']; ?></a></h3>
-                <div class="lot__state">
-                    <div class="lot__rate">
-                        <span class="lot__amount"><?=priceform($lot['price'])?></span>
-                        <span class="lot__cost"><?=priceform($lot['price'])?></span>
-                    </div>
-                    <div class="lot__timer timer">
-                            12:23
-                    </div>
-                </div>
-            </div>
-        </li>
+          <li class="promo__item promo__item--boards">
+              <a class="promo__link" href="pages/all-lots.html<?=$index;?>"><?= $category;?></a>
+          </li>
+      <?php endforeach; ?>
+      </ul>
+  </section>
+  <section class="lots">
+      <div class="lots__header">
+          <h2>Открытые лоты</h2>
+      </div>
+      <ul class="lots__list">
+          <?php foreach ($lot_list as $lot ): ?>
+          <li class="lots__item lot">
+              <div class="lot__image">
+                  <img src="<?=$lot['picture']; ?>" width="350" height="260" alt="">
+              </div>
+              <div class="lot__info">
+                  <span class="lot__category"><?=$lot['category']; ?></span>
+                  <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$lot['title']; ?></a></h3>
+                  <div class="lot__state">
+                      <div class="lot__rate">
+                          <span class="lot__amount"><?=priceform($lot['price'])?></span>
+                          <span class="lot__cost"><?=priceform($lot['price'])?></span>
+                      </div>
+                      <div class="lot__timer timer">
+                            <?=$hours?>:<?=$minutes?>
+                      </div>
+                  </div>
+              </div>
+          </li>
         <?php endforeach; ?>
-    </ul>
-</section>
+      </ul>
+  </section>
 </main>
