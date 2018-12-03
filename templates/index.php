f1b8df0556afc433217ed30c44fe3049003725e3
Sergey <ser-stulin20116@yandex.ru>
2018-12-04 01:36:17 +0500

    Чтение и показ записей из MySQL (Последние изменения)

--- templates/index.php
+++ templates/index.php
@@ -17,15 +17,15 @@
         <?php foreach ($lots as $lot ): ?>
         <li class="lots__item lot">
             <div class="lot__image">
-                <img src="<?=$lot['`avatar`']; ?>" width="350" height="260" alt="">
+                <img src="<?=$lot['img']; ?>" width="350" height="260" alt="">
             </div>
             <div class="lot__info">
-                <span class="lot__category"><?=$lot['name']; ?></span>
+                <span class="lot__category"><?=$lot['title']; ?></span>
                 <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$lot['title']; ?></a></h3>
                 <div class="lot__state">
                     <div class="lot__rate">
-                        <span class="lot__amount"><?=priceform($lot['price'])?></span>
-                        <span class="lot__cost"><?=priceform($lot['`start_price`'])?></span>
+                        <span class="lot__amount"><?=priceform($lot['start_price'])?></span>
+                        <span class="lot__cost"><?=priceform($lot['start_price'])?></span>
                     </div>
                     <div class="lot__timer timer">
                             12:23
